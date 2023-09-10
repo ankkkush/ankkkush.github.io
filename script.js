a9bff12617d047d807614906c8b59ae17bde37d8
Ankush Attri <ankushattri1945@gmail.com>
2023-09-11 03:09:04 +0530

    main

---
+++ script.js
@@ -0,0 +1,16 @@
+// JavaScript to toggle dropdown
+const dropdownButton = document.querySelector('.dropbtn');
+const dropdownContent = document.querySelector('.menu-content');
+
+dropdownButton.addEventListener('click', () => {
+    dropdownContent.classList.toggle('show');
+});
+
+// Close the dropdown if the user clicks outside of it
+window.addEventListener('click', (event) => {
+    if (!event.target.matches('.dropbtn')) {
+        if (dropdownContent.classList.contains('show')) {
+            dropdownContent.classList.remove('show');
+        }
+    }
+});
